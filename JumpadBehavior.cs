d3b5f189fc40730444b78ea7eba55629b05888b2
LietuvisKalba <mantasling@gmail.com>
2019-01-01 09:35:52 +0000

    Jump pads

---
+++ JumpadBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpadBehavior : MonoBehaviour {
+
+    private float jumpDelay;
+    private float nextJump;
+    private bool hasSteppedOnPad;
+
+    void Start () {
+        jumpDelay = 0.2f;
+        hasSteppedOnPad = false;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+
+        
+    }
+
+    public void SetLaunchReady(bool hasStepped)
+    {
+        hasSteppedOnPad = hasStepped;
+    }
+    
+    public bool GetLaunchReady()
+    {
+        return hasSteppedOnPad;
+    }
+}
---
+++ PlayerMarkers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMarkers : MonoBehaviour {
+
+    public GameObject mark;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(mark, transform.position, Quaternion.identity);
+        }
+    }
+}
